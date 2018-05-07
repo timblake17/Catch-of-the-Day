2a303c75c268c007f9b126939c9e520a682c6b1a
Tim Blake <timblake17@hotmail.com>
2018-05-07 11:17:59 -0400

    app working, going to persist state on firebase

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
+
 
 
 
@@ -21,14 +24,37 @@ class App extends React.Component{
     //set new fishes object to state
     this.setState({fishes})
   }
+
+  loadSampleFishes=() => {
+    this.setState({ fishes: sampleFishes });
+  };
+
+  addToOrder = (key) => {
+    //take copy of state
+    const order = {...this.state.order};
+    //add to order or update number in order
+    order[key] = order[key] + 1 || 1;
+    ///cal setstate to update our state object
+    this.setState({ order });
+  }
   render(){
     return(
       <div className="catch-of-the-day">
         <div className="menu">
         <Header tagline="Fresh Seafood Market"/>
+        <ul className="fishes">
+          {Object.keys(this.state.fishes).map(key =>(
+            <Fish
+              key={key}
+              index={key}
+              details={this.state.fishes[key]}
+              addToOrder={this.addToOrder}
+            />
+          ))}
+        </ul>
         </div>
-      <Order/>
-      <Inventory addFish={this.addFish}/>
+      <Order fishes={this.state.fishes} order={this.state.order}/>
+      <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
       </div>
 
     )
